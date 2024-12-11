fbcc11caa965ef5cc7cb8211e525c4412e6925c5
BugHunter-X <askzenai@gmail.com>
2024-12-11 19:52:06 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+// The server might not be able to start if port 8080 is already in use by another process.
